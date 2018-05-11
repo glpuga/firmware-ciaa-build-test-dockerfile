e636aec9bde6df4e36665c34c6b224c392803c9b
Gerardo Puga <glpuga@gmail.com>
2018-05-11 11:31:46 -0300

    Failing test

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,8 +3,8 @@
 git clone --recursive https://github.com/ciaa/Firmware.git
 cd Firmware
 cd modules/rtos
-git checkout master
-cd ../..
+#git checkout master
+#cd ../..
 
 cp ../Makefile.mine .
 
@@ -12,4 +12,12 @@ if [ $# -eq 0 ]; then
     /bin/bash
 else
     make $@
+
+    TESTRESULT=`cat out/rtos/doc/ctest/ctestSummary.log | grep -c "FAILED: 0"`
+
+    if [ $TESTRESULT -eq 0 ]; then
+	     exit 1
+    fi
 fi
+
+exit 0
